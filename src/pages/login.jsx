25d3e39b11bb4de881304448b37e2a720e39018e
Sadini <jasingha2002sadninipunika@gamil.com>
2025-10-21 00:21:09 +0530

    login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,15 +1,20 @@
 export default function LoginPage(){
     return(
-        <div className="w-full h-[50px] bg-red flex">
-            <h1 className="font-bold text-[50px] text-blue">Sadini</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nobis labore commodi eveniet excepturi quae nihil vitae temporibus doloribus suscipit? Blanditiis obcaecati soluta perspiciatis debitis commodi provident, nisi repellat. Fugiat!</p>
-           <button className="text-dark white text-2xl font-semibold bg-blue-100 hover:bg-blue-100 px-8 py-3 rounded-2xl shadow-md transition duration-100 ease-in-out">
-  Login
-</button>
-<button className="text-blue-600 text-2xl font-semibold border-2 border-blue-600 hover:bg-blue-600 hover:text-white px-8 py-3 rounded-2xl transition duration-300">
-  gin
-</button>
+        <div className="w-full h-screen bg-[url('/labo.jpg')] flex">
+          <div className="w-[50%] h-full ">
 
+          </div>
+          <div className="w-[50%] h-full flex justify-center item-center">
+            <div className="w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl"/>
+            <input className="w-[300px] h-[50px] bg-[#c3efe9] rounded-[20px]"/>
+
+            <input type="password" className="w-[300px] h-[50px] bg-[#c3efe9] rounded-[20px]"/>
+            
+            
+
+
+          </div>
+            
 
             
 
